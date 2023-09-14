dc76e739aad1afb89e88c826363f331686d4df3a
gynlee0904 <gynleedvlop@gmail.com>
2023-09-14 18:00:12 +0900

    [ADD]23/09/14-분실물 게시판 VO추가

--- src/main/java/kr/co/chazm/lost/domain/LostLike.java
+++ src/main/java/kr/co/chazm/lost/domain/LostLike.java
@@ -1,5 +1,37 @@
 package kr.co.chazm.lost.domain;
 
 public class LostLike {
+	private int likeNo;
+	private int refLostNo;
+	private String memberId;
+	
+	
+	
+	
+	public int getLikeNo() {
+		return likeNo;
+	}
+	public void setLikeNo(int likeNo) {
+		this.likeNo = likeNo;
+	}
+	public int getRefLostNo() {
+		return refLostNo;
+	}
+	public void setRefLostNo(int refLostNo) {
+		this.refLostNo = refLostNo;
+	}
+	public String getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+	
+	@Override
+	public String toString() {
+		return "LostLike [likeNo=" + likeNo + ", refLostNo=" + refLostNo + ", memberId=" + memberId + "]";
+	}
+	
+	
 
 }
